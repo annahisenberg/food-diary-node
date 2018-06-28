fe25966654fe63e5b4bdf9d87515236f429939b7
Annah Isenberg <annahisenberg@gmail.com>
2018-06-28 13:31:15 -0400

    Add "service" to travis

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,62 +1,60 @@
-function onSignUpClick() {
-    $('.sign-up').click(function() {
-        $('form').submit((e) => {
-            e.preventDefault();
-            const email = $('.js-email-field').val();
-            const password = $('.js-password-field').val();
+// function onSignUpClick() {
+//     $('.sign-up').click(function() {
+//         $('form').submit((e) => {
+//             e.preventDefault();
+//             const email = $('.js-email-field').val();
+//             const password = $('.js-password-field').val();
 
-            console.log(email, password);
+//             console.log(email, password);
 
 
-            $.ajax({
-                url: '/api/users',
-                method: 'POST',
-                data: {
-                    email: email,
-                    password: password,
-                },
-                success: (response) => {
-                    console.log(response);
+//             $.ajax({
+//                 url: '/api/users',
+//                 method: 'POST',
+//                 data: {
+//                     email: email,
+//                     password: password,
+//                 },
+//                 success: (response) => {
+//                     console.log(response);
 
-                    sessionStorage.setItem('token', response.token);
-                    location.href = '/entries.html';
-                },
-                error: (err) => {
-                    // renderError();
-                    console.log(err);
-                }
-            });
-        });
-    });
-}
+//                     sessionStorage.setItem('token', response.token);
+//                     location.href = '/entries.html';
+//                 },
+//                 error: (err) => {
+//                     // renderError();
+//                     console.log(err);
+//                 }
+//             });
+//         });
+//     });
+// }
 
 function onLoginClick() {
-    $('.login-link').click(function() {
-        $('form').submit((e) => {
-            e.preventDefault();
-            const email = $('.js-email-field').val();
-            const password = $('.js-password-field').val();
+    $('#login-form').submit((e) => {
+        e.preventDefault();
+        const email = $('.js-email-field').val();
+        const password = $('.js-password-field').val();
 
-            console.log(email, password);
+        console.log(email, password);
 
-            $.ajax({
-                url: '/api/login',
-                method: 'POST',
-                data: {
-                    email: email,
-                    password: password,
-                },
-                success: (response) => {
-                    console.log(response);
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            data: {
+                email: email,
+                password: password,
+            },
+            success: (response) => {
+                console.log(response);
 
-                    sessionStorage.setItem('token', response.token);
-                    location.href = '/entries.html';
-                },
-                error: (err) => {
-                    // renderError();
-                    console.log(err);
-                }
-            });
+                sessionStorage.setItem('token', response.token);
+                location.href = '/entries.html';
+            },
+            error: (err) => {
+                // renderError();
+                console.log(err);
+            }
         });
     });
 }
@@ -89,18 +87,12 @@ function addSelectClassToLoginLink() {
     });
 }
 
-function burgerNav() {
-    $('.toggle-nav').click(function() {
-        $('nav ul').toggleClass("show-nav");
-        $('nav ul li').addClass("nav-background");
-    })
-}
 
 //  on page load do this
 $(function() {
     animateArrowIcon();
-    burgerNav();
     addSelectClassToLoginLink();
     addSelectClassToSignUp();
     onLoginClick();
+    // onSignUpClick();
 })
